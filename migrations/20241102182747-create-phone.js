1d0b5a867bc0d8617097cc30cef189f521370f50
unknown <mrsolomka2447@gmail.com>
2024-11-02 21:22:08 +0200

    create phone, edit model

---
+++ migrations/20241102182747-create-phone.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Phones', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      year_of_prod: {
+        type: Sequelize.DATEONLY
+      },
+      ram: {
+        type: Sequelize.INTEGER
+      },
+      cpu: {
+        type: Sequelize.STRING
+      },
+      screen_diagonal: {
+        type: Sequelize.STRING
+      },
+      have_nfc: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Phones');
+  }
+};
---
+++ models/phone.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Phone extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  //npx sequelize model:create --name "Phone" --attributes model:string,brand:string,year_of_prod:dateonly,ram:integer,cpu:string,screen_diagonal:string,have_nfc:boolean
+  Phone.init(
+    {
+      model: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        validate: { is: /^[A-Z][a-z]$/, len: [2, 40] },
+      },
+      brand: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        validate: { is: /^[A-Z][a-z]$/, len: [2, 40] },
+      },
+      year_of_prod: { type: DataTypes.DATEONLY, validate: { isDate: true } },
+      ram: { type: DataTypes.INTEGER, validate: { isInt: true } },
+      cpu: { type: DataTypes.STRING(40), validate: { len: [2, 40] } },
+      screen_diagonal: { type: DataTypes.STRING(8), validate: { len: [1, 8] } },
+      have_nfc: { type: DataTypes.BOOLEAN },
+    },
+    {
+      sequelize,
+      modelName: "Phone",
+    }
+  );
+  return Phone;
+};
